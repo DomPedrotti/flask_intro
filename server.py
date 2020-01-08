158b070575995fb65b6d3408a8ce17d64add97f8
Dominic Pedrotti <pedrotti.dom@gmail.com>
2020-01-08 09:10:58 -0600

    more experiments on different ways of web page development

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from random import randint
+from model import predict
 
 app = Flask(__name__)
 
@@ -32,5 +33,45 @@ def n_dice(ndice):
 @app.route('/html-roll/<int:ndice>')
 def html_roll(ndice):
     rolls = [randint(1,6) for _ in range(ndice)]
-    
+
     return render_template('roll-dice.html', rolls = rolls)
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/greeting-form')
+def form():
+    return render_template('greeting-form.html')
+
+@app.route('/make-greeting', methods=['POST'])
+def handle_form_submission():
+    title = request.form['title']
+    name = request.form['name']
+
+    concated = title + ' ' + name
+
+    greeting = 'Hello'
+
+    if concated != ' ':
+        greeting += ' ' + concated
+    
+    greeting += '!'
+
+    return render_template('greeting-result.html', greeting=greeting)
+
+@app.route('/ham-spam-request')
+def get_ham_spam():
+    return render_template("ham-spam-request.html")
+
+@app.route('/ham-spam-result', methods=['POST'])
+def verify_ham_spam():
+    message = request.form['message']
+    
+    response = predict(message)
+
+    if response == 'spam':
+        result = 'Fuck on outta here with that spam'
+    else:
+        result = 'Nah, you good fam, that shit ham'
+    return render_template('ham-spam-result.html', result = result)
